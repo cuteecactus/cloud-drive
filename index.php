ca8dc9dd204c33cf3d09fcd0dace9c8a918cba98
cuteecactus <abudah181@gmail.com>
2025-06-29 16:00:58 +0500

    home files grid view - full, home file list view - partial

--- index.php
+++ index.php
@@ -6,26 +6,111 @@
     <div id="home-files-head">
         <h1>Your Files</h1>
         <div class="files-grid-layout-options">
-            <svg class="grid-view-list active-grid-view" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+            <svg class="grid-view-list " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M2 4C2 3.44772 2.44772 3 3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4ZM4 5V19H20V5H4ZM6 7H8V9H6V7ZM8 11H6V13H8V11ZM6 15H8V17H6V15ZM18 7H10V9H18V7ZM10 15H18V17H10V15ZM18 11H10V13H18V11Z"></path>
             </svg>
-            <svg class="grid-view-grid" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+            <svg class="grid-view-grid active-grid-view" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M21 3C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3H21ZM11 13H4V19H11V13ZM20 13H13V19H20V13ZM11 5H4V11H11V5ZM20 5H13V11H20V5Z"></path>
             </svg>
         </div>
     </div>
 
-    <div id="home-files"></div>
+    <div id="home-files">
+        <!-- <div id="home-nothing">
+            <p>You have not uploaded any files yet.</p>
+        </div> -->
+
+        <div id="home-files-display" class="flex">
+            <div class="home-file">
+                <div class="home-file-thumbnail">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M3 8L9.00319 2H19.9978C20.5513 2 21 2.45531 21 2.9918V21.0082C21 21.556 20.5551 22 20.0066 22H3.9934C3.44476 22 3 21.5501 3 20.9932V8ZM10 4V9H5V20H19V4H10Z"></path>
+                    </svg>
+                </div>
+                <div class="home-file-details">
+                    <p>File Name <span>- 1.3GB</span></p>
+                </div>
+            </div>
+            <div class="home-file">
+                <div class="home-file-thumbnail">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M3 8L9.00319 2H19.9978C20.5513 2 21 2.45531 21 2.9918V21.0082C21 21.556 20.5551 22 20.0066 22H3.9934C3.44476 22 3 21.5501 3 20.9932V8ZM10 4V9H5V20H19V4H10Z"></path>
+                    </svg>
+                </div>
+                <div class="home-file-details">
+                    <p>File Name <span>- 1.3GB</span></p>
+                </div>
+            </div>
+            <div class="home-file">
+                <div class="home-file-thumbnail">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M3 8L9.00319 2H19.9978C20.5513 2 21 2.45531 21 2.9918V21.0082C21 21.556 20.5551 22 20.0066 22H3.9934C3.44476 22 3 21.5501 3 20.9932V8ZM10 4V9H5V20H19V4H10Z"></path>
+                    </svg>
+                </div>
+                <div class="home-file-details">
+                    <p>File Name <span>- 1.3GB</span></p>
+                </div>
+            </div>
+            <div class="home-file">
+                <div class="home-file-thumbnail">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M3 8L9.00319 2H19.9978C20.5513 2 21 2.45531 21 2.9918V21.0082C21 21.556 20.5551 22 20.0066 22H3.9934C3.44476 22 3 21.5501 3 20.9932V8ZM10 4V9H5V20H19V4H10Z"></path>
+                    </svg>
+                </div>
+                <div class="home-file-details">
+                    <p>File Name <span>- 1.3GB</span></p>
+                </div>
+            </div>
+            <div class="home-file">
+                <div class="home-file-thumbnail">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M3 8L9.00319 2H19.9978C20.5513 2 21 2.45531 21 2.9918V21.0082C21 21.556 20.5551 22 20.0066 22H3.9934C3.44476 22 3 21.5501 3 20.9932V8ZM10 4V9H5V20H19V4H10Z"></path>
+                    </svg>
+                </div>
+                <div class="home-file-details">
+                    <p>File Name <span>- 1.3GB</span></p>
+                </div>
+            </div>
+            <div class="home-file">
+                <div class="home-file-thumbnail">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M3 8L9.00319 2H19.9978C20.5513 2 21 2.45531 21 2.9918V21.0082C21 21.556 20.5551 22 20.0066 22H3.9934C3.44476 22 3 21.5501 3 20.9932V8ZM10 4V9H5V20H19V4H10Z"></path>
+                    </svg>
+                </div>
+                <div class="home-file-details">
+                    <p>File Name <span>- 1.3GB</span></p>
+                </div>
+            </div>
+            <div class="home-file">
+                <div class="home-file-thumbnail">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M3 8L9.00319 2H19.9978C20.5513 2 21 2.45531 21 2.9918V21.0082C21 21.556 20.5551 22 20.0066 22H3.9934C3.44476 22 3 21.5501 3 20.9932V8ZM10 4V9H5V20H19V4H10Z"></path>
+                    </svg>
+                </div>
+                <div class="home-file-details">
+                    <p>File Name <span>- 1.3GB</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 
 <script>
     document.querySelector('.grid-view-list').addEventListener('click', () => {
+        document.querySelector('.grid-view-list').classList.add('active-grid-view');
+        document.querySelector('.grid-view-grid').classList.remove('active-grid-view');
+        document.querySelector("#home-files-display").classList.add('flex')
+        document.querySelector("#home-files-display").classList.remove('grid')
         document.querySelectorAll('.file').forEach(file => {
             file.style.display = 'flex';
         });
     });
     document.querySelector('.grid-view-grid').addEventListener('click', () => {
+        document.querySelector('.grid-view-grid').classList.add('active-grid-view');
+        document.querySelector('.grid-view-list').classList.remove('active-grid-view');
+        document.querySelector("#home-files-display").classList.remove('flex')
+        document.querySelector("#home-files-display").classList.add('grid')
         document.querySelectorAll('.file').forEach(file => {
             file.style.display = 'none';
         });
